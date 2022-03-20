431262d0eec31998c5f77294ffc3d6d417beb113
emilio <emilio113kariuki@gmail.com>
2022-03-20 14:13:36 +0300

    March 20, 2022 2:13 PM

--- lib/Api.dart
+++ lib/Api.dart
@@ -1,15 +1,16 @@
 import 'package:dio/dio.dart';
 import "package:flutter/material.dart";
 import 'package:movie_lucifer/model.dart';
+import "package:http/http.dart" as http;
 
 class Api {
-  Dio dio = Dio();
+  // Dio dio = Dio();
 
   Future<List<User>?> showQuote() async {
     var response =
-        await dio.get("https://lucifer-quotes.vercel.app/api/quotes/500");
+        await http.get(Uri.parse("https://lucifer-quotes.vercel.app/api/quotes/500"));
     if (response.statusCode == 200) {
-      var json = response.data;
+      var json = response.body;
       return userFromJson(json);
     } else {
       return null;
